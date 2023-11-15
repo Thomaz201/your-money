6b897860b843ca5716ea3b86dda5c244c35f4507
Lucas <lucasthomazr@hotmail.com>
2023-11-14 22:55:56 -0400

    Inital commit

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,49 @@
+export const defaultTheme = {
+  white: '##F8F8F2',
+
+  'gray-100': '#EEEEF2',
+  'gray-300': '#DDDEE6',
+  'gray-400': '#989CB3',
+  'gray-500': '#767B99',
+  'gray-600': '#4D5168',
+  'gray-700': '#343646',
+  'gray-800': '#282A36',
+  'gray-900': '#14151A',
+
+  'purple-300': '#BD93F9',
+  'purple-500': '#9E5FF6',
+  'purple-700': '#610DD7',
+
+  'pink-500': '#FF79C6',
+
+  'green-300': '#83FBA1',
+  'green-500': '#50FA7B',
+  'green-700': '#05A12C',
+
+  'red-300': '#FF7B7B',
+  'red-500': '#FF5555',
+  'red-700': '#DC0000',
+
+  'yellow-500': '#F1FA8C',
+} as const
+
+export const originalTheme = {
+  white: '#fff',
+
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'green-300': '#00B37E',
+  'green-500': '#00875F',
+  'green-700': '#015F43',
+
+  'red-300': '#F75A68',
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+} as const
